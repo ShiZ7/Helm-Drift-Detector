cdbb8cf607f12a777ec2f51a71e8968cc8c0e60d
Shivansh Shanker Gupta <98808480+ShiZ7@users.noreply.github.com>
2025-08-26 12:22:00 +0530

    Update log_to_csv.sh

--- Helm-Drift-Detector/Helm-Drift-Detection/Helm-Detection/scripts/log_to_csv.sh
+++ Helm-Drift-Detector/Helm-Drift-Detection/Helm-Detection/scripts/log_to_csv.sh
@@ -6,7 +6,7 @@ CSV_OUTPUT_FILE="${2:-drift_history.csv}"
 # Initialize CSV if 1t doesn't exist
 
 if [ ! -f "$CSV_OUTPUT_FILE" ]; then
-echo "Timestamp, Resource, Field, Local, Live" > "$CSV_OUTPUT_FILE" 
+echo "Timestamp, Resource, Field, PR , Cluster" > "$CSV_OUTPUT_FILE" 
 fi
 
 # Extract DRIFI Lines trom log and convert to Csv
@@ -16,8 +16,8 @@ grep "DRIFT:" "$DRIFT_OUTPUT_FILE" | while IFS= read -r line; do
   field=$(echo "Sline" | cut -d ':' -f2 | cut -d '(' -f1 | xargs) 
   values=slecho "Sline" | grep -op '(Local=,*?, Live=,*?\' | sed "s/l()J//g")
   
-  local_val=$(echo "$values" | sed -n 's/Local=\(.*\), Live=.*/\1/p')
-  live_val=$(echo "$values" | sed -n 's/.*Live=\(.*\)/\1/p') 
+  PR_val=$(echo "$values" | sed -n 's/PR=\(.*\), Cluster=.*/\1/p')
+  Cluster_val=$(echo "$values" | sed -n 's/.*Cluster=\(.*\)/\1/p') 
   
-  echo "$(date), HPA/$field, $field, $local_val, $live_val" >> "$CSV_OUTPUT_FILE"
+  echo "$(date), HPA/$field, $field, $PR_val, $Cluster_val" >> "$CSV_OUTPUT_FILE"
 done
